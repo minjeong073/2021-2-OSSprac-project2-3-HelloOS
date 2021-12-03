7eb6987e75481cdd22bbc131d0fcae94dfbb378b
김민정 <91234288+minjeong073@users.noreply.github.com>
2021-12-03 17:35:15 +0900

    Update serve.py

--- app/serve.py
+++ app/serve.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('reservation.html')
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
